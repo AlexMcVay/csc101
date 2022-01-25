1772170fffec2423383878e3132f134c05b0453c
AlexMcVay <72508887+AlexMcVay@users.noreply.github.com>
2022-01-25 12:31:39 -0500

    Add files via upload

---
+++ save.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print ("Hello World")
+
+
+# In[2]:
+
+
+import turtle
+
+
+# In[3]:
+
+
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(120)
+turtle.circle(10)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(20)
+turtle.circle(10)
+turtle.left(180)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(170)
+
+
+# In[4]:
+
+
+turtle.left(90)
+turtle.forward(200)
+
+
+# In[ ]:
+
+
+
+
